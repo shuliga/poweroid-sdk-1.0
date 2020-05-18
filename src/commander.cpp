54e79da5b640baa5065d179570a11d04145d4f84
shuliga <shuliga@gmail.com>
2020-05-18 21:52:32 +0300

    'set_flags' command added

--- src/commander.cpp
+++ src/commander.cpp
@@ -97,6 +97,12 @@ void Commander::listen() {
                     }
                 }
 
+                if (cmd.startsWith(cu.cmd_str.CMD_SET_FLAGS)) {
+                    if (getValIndex() > 1){
+                        PWR_FLAGS = strtol(cmd.substring(getValIndex()).c_str(), NULL, 2);
+                    }
+                }
+
                 if (cmd.startsWith(cu.cmd_str.CMD_GET_ALL_PROP)) {
                     for (uint8_t i = 0; i < ctx->props_size; i++) {
                         printCmdResponse(cmd, printProperty(i));
